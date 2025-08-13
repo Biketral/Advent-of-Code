3a35989b8539132cc5339698566ef5ea75fa947c
Biketral <112985056+Biketral@users.noreply.github.com>
2025-08-13 17:23:34 +0200

    Update and rename DayTwo.java to Day2.java

--- 2024/Day2.java
+++ 2024/Day2.java
@@ -3,7 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException; 
 import java.util.Scanner; 
 
-public class DayTwo {
+public class Day2 {
 	public static void main(String[] args) {
     
     try {
